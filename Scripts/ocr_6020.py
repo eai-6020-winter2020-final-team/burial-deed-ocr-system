6887671cce5979967647dfaeb248737b81331c34
不知 <Alienoftt@gmail.com>
2020-03-21 01:34:53 -0400

    Revert "module line 75"
    This reverts commit d8db0f637603c1d620e1f8ce3d79d93a827217bb.

--- Scripts/ocr_6020.py
+++ Scripts/ocr_6020.py
@@ -72,8 +72,7 @@ def img_to_arr(img, x, y):
     Convert image into array
     update: 2020.3.20
     """
-    if len(img.shape) > 2:
-        img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)).convert('L')
+    img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)).convert('L')
     if img.size[0] != x or img.size[1] != y:
         img = img.resize((x, y))
 
